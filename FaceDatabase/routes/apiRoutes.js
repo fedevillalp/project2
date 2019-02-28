1494f0f752637b90fb3138c4ca5b188440080e70
Federico <federico@Federicos-MacBook-Pro.local>
2019-02-28 14:48:55 -0800

    Text box, button can upload a link and get face information from Microsoft

--- FaceDatabase/routes/apiRoutes.js
+++ FaceDatabase/routes/apiRoutes.js
@@ -5,14 +5,14 @@ var db = require("../models");
 const request = require('request');
 
 // Replace <Subscription Key> with your valid subscription key.
-const subscriptionKey = '';
+const subscriptionKey = '651740b5ffa343dc82aa1da95c88fde8';
 
 // You must use the same location in your REST call as you used to get your
 // subscription keys. For example, if you got your subscription keys from
 // westus, replace "westcentralus" in the URL below with "westus".
 const uriBase = 'https://westus.api.cognitive.microsoft.com/face/v1.0/detect';
 
-const imageUrl =
+var imageUrl =
       'https://images.pexels.com/photos/1139743/pexels-photo-1139743.jpeg?cs=srgb&dl=adult-confidence-elderly-man-1139743.jpg&fm=jpg';
      
 // Request parameters.
@@ -34,32 +34,38 @@ const options = {
 };
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+  // // Get all examples
+  // app.get("/api/examples", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.json(dbExamples);
+  //   });
+  // });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // // Create a new example
+  // app.post("/api/examples", function(req, res) {
+  //   db.Example.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // // Delete an example by id
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 
   //When the client gets this route , a POST is made to the FACE API using 
   //the request library.
 
-  app.get("/api/faces", function(req, res) {
+
+  app.post("/api/face/upload", function(req, res) {
+    console.log('this is  app.post for /api/face/upload');
     
+    imageUrl = JSON.stringify(req.body.link);
+    options.body = '{"url": ' +     imageUrl +   '}'
+    
+    // This code provided my Microsoft to make request
     request.post(options, (error, response, body) => {
       if (error) {
         console.log('Error: ', error);
@@ -68,9 +74,10 @@ module.exports = function(app) {
       let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
       console.log('JSON Response\n');
       console.log(jsonResponse);
-      res.json(body);
+      res.json(jsonResponse);
       
     });
+    
 
   });
   
